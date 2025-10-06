8a68d215c91ec41ce4c5297a1995c0d8ebb4438e
Jaolmos <jose.a.olmosmartinez@gmail.com>
2025-10-06 23:00:08 +0200

    Revert "fix: deshabilitar logging a archivo en producción para evitar errores de permisos"
    This reverts commit de8d4e27c496abc37305f58a72e08d56bbabba62.

--- slidemotion/settings/production.py
+++ slidemotion/settings/production.py
@@ -92,7 +92,15 @@ X_FRAME_OPTIONS = 'DENY'
 # CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'")
 # CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
 
-# Logging for production (solo consola, sin archivo para evitar problemas de permisos)
+# Logging for production
+LOGGING['handlers']['file'] = {
+    'class': 'logging.handlers.RotatingFileHandler',
+    'filename': os.getenv('LOG_FILE', str(BASE_DIR / 'logs' / 'django.log')),
+    'maxBytes': 1024*1024*15,  # 15MB
+    'backupCount': 10,
+    'formatter': 'verbose',
+}
+
 LOGGING['handlers']['console']['level'] = 'WARNING'
 LOGGING['loggers']['django']['level'] = 'WARNING'
 LOGGING['loggers']['apps']['level'] = 'INFO'
